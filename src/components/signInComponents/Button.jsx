0e4b4ec47a2834e3212e020f8d75ccee36db7ac0
faraaz <faraaz.husayn@gmail.com>
2024-02-22 21:31:37 +0530

    visual aspects of signup/signin page ready

---
+++ src/components/signInComponents/Button.jsx
@@ -0,0 +1,6 @@
+
+export function Button({label}) {
+    return <div>
+        <button className="my-5 text-lg text-white bg-black w-full rounded-full px-24 py-3 font-semibold">{label}</button>
+    </div>
+}
