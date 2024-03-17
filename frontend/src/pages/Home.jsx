baaadf23f5e07c8e69531d07e6562ef67e8c77a4
ZohaibAlam-0706 <zohaibalam0706@gmail.com>
2024-03-17 22:50:19 +0530

    Added Post Wrapper

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,18 @@
 import { Topbar } from "../components/global/Topbar";
 import { Sidebar } from "../components/global/Sidebar";
 import { AddPost } from "../components/Home/AddPost";
+import { Post } from "../components/Home/Post";
+import { Users } from "../../dummyData";
 
 export function Home() {
+    const CurrentUser = Users.Users[0];
     return <div>
         <Topbar />
         <div className="grid homepage-grid items-start justify-items-center">
             <Sidebar />
             <div className="w-full">
                 <AddPost />
+                <Post ProfilePicture={CurrentUser.ProfilePicture}/>
             </div>
             <div className="bg-blue-400">Rightbar</div>
         </div>
