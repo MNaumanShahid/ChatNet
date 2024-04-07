01e55959f6a849909cbc3623e061be776fe060cd
Mohammad Nauman Shahid <naumanshahid.work@gmail.com>
2024-04-07 22:30:40 +0530

    Making changes for deployment

--- backend/app.py
+++ backend/app.py
@@ -556,6 +556,7 @@ def check_like(post_id):
     except Exception as e:
         return jsonify({'message': str(e)}), 400
 
+@cross_origin()
 @app.route("/search_users/<filter>", methods=['GET'])
 @jwt_required()
 def search_users(filter):
