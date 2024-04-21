b9591cc413d8fdc8ab9baf80ee49ded25369cd66
ZohaibAlam-0706 <zohaibalam0706@gmail.com>
2024-04-21 21:45:10 +0530

    Completed messenger page with backend integration

--- frontend/src/components/messenger/User.jsx
+++ frontend/src/components/messenger/User.jsx
@@ -1,8 +1,17 @@
-export function User({profilePicture, firstName, lastName, username, setActiveUser}){
+import { useEffect } from "react";
+
+export function User({ profilePicture, firstName, lastName, username, setActiveUser, getMessages, activeUser }) {
+    useEffect(() => {
+        if (activeUser) {
+          getMessages();
+        }
+      }, [activeUser]);
+    const handleClick = async () => {
+        await setActiveUser(username);
+    };
+
     return (
-        <div onClick={() => {
-            setActiveUser(username);
-        }} className="flex justify-start hover:bg-primary hover:text-white border-gray-900 h-16 cursor-pointer">
+        <div onClick={handleClick} className="flex justify-start hover:bg-primary hover:text-white border-gray-900 h-16 cursor-pointer">
             <div>
                 <img className="rounded-full h-12 mt-2 mx-3" src={profilePicture} alt="User" />
             </div>
@@ -10,5 +19,5 @@ export function User({profilePicture, firstName, lastName, username, setActiveUs
                 {firstName} {lastName}
             </div>
         </div>
-    )
+    );
 }
